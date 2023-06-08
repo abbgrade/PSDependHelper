0c6d0959d316f9e4092535f7eef360805ea30a00
Steffen Kampmann <steffen.kampmann@gmail.com>
2023-06-08 19:00:24 +0200

    fixed export

--- test/Export-DependencyFile.Tests.ps1
+++ test/Export-DependencyFile.Tests.ps1
@@ -8,8 +8,13 @@ Describe Export-DependencyFile {
     It works {
         $DependencyFile = "$TestDrive/.depends.psd1"
         @(
-            (New-Object -TypeName Microsoft.PowerShell.Commands.ModuleSpecification 'First')
+            (New-Object -TypeName Microsoft.PowerShell.Commands.ModuleSpecification 'First'),
+            (New-Object -TypeName Microsoft.PowerShell.Commands.ModuleSpecification 'Second')
         ) | Export-DependencyFile -Path $DependencyFile
         $DependencyFile | Should -Exist
+        $Dependencies = $DependencyFile | Import-Psd 
+        $Dependencies | Should -HaveCount 1
+        $Dependencies.First | Should -Be latest
+        $Dependencies.Second | Should -Be latest
     }
 }
