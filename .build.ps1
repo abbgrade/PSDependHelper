4e0de39945356a122270b0e18a056e6a791abd67
Steffen Kampmann <steffen.kampmann@gmail.com>
2023-06-08 17:27:21 +0200

    added required tasks

--- .build.ps1
+++ .build.ps1
@@ -2,3 +2,7 @@ $ModuleName = 'PSDependHelper'
 
 . $PSScriptRoot/tasks/Build.Tasks.ps1
 . $PSScriptRoot/tasks/PsBuild.Tasks.ps1
+
+task InstallBuildDependencies -Jobs {}
+task InstallTestDependencies -Jobs {}
+task InstallReleaseDependencies -Jobs {}
