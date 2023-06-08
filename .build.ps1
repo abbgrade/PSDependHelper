ddbce89d4faf1334eedad2573e08a242b94a87b5
Steffen Kampmann <steffen.kampmann@gmail.com>
2023-06-08 17:30:42 +0200

    added dependency

--- .build.ps1
+++ .build.ps1
@@ -3,6 +3,8 @@ $ModuleName = 'PSDependHelper'
 . $PSScriptRoot/tasks/Build.Tasks.ps1
 . $PSScriptRoot/tasks/PsBuild.Tasks.ps1
 
-task InstallBuildDependencies -Jobs {}
+task InstallBuildDependencies -Jobs {
+    Install-Module PsdKit
+}
 task InstallTestDependencies -Jobs {}
 task InstallReleaseDependencies -Jobs {}
